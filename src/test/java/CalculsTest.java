ab9e9715d6380e8146010b4d2739bc2003d05ade
Rodrigues Matthieu <matthieu.rodrigues@epsi.fr>
2020-12-14 16:41:20 +0100

    update Pom

--- src/test/java/CalculsTest.java
+++ src/test/java/CalculsTest.java
@@ -1,52 +1,24 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.Test;
 
 public class CalculsTest {
 
-    private Calculs c1 = null;
-    private Calculs c2 = null;
-    private Calculs c3 = null;
-
-    @BeforeEach
-    public void setup() throws Exception
-    {
-        c1 = new Calculs(1,2);
-        c2 = new Calculs(10,200);
-        c3 = new Calculs(100,200);
-    }
-
-
     @Test
-    void testMultiplier() {
-        if (c1.multiplier() != 2)
-        {
-            fail("Méthode multiplier incorrecte");
-        }
+    public void testerMultipliser(){
+        Calculs calculs = new Calculs(2,4);
 
-        assertEquals(c1.multiplier(), 2);
+        int valMult = calculs.multiplier();
+
+        assertEquals(valMult, 8);
     }
 
     @Test
-    void testAdditionner() {
-        if (c1.additionner() != 3)
-        {
-            fail("Méthode additionner incorrecte");
-        }
+    public void testerAdditionner(){
+        Calculs calculs = new Calculs(2,4);
 
-        assertEquals(c1.additionner(), 3);
-    }
+        int valAdd = calculs.additionner();
 
-    @Test
-    void testSoustraire() {
-        if (c1.soustraire() != -1)
-        {
-            fail("Methode soustraire incorrecte");
-        }
-
-        assertEquals(c1.soustraire(), -1);
+        assertEquals(valAdd, 6);
     }
 }
